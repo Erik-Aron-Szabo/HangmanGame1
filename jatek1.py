e819e53d3fabac77e1cbfa1715fef19d4567ba63
Erik Aron Szabo <erik.aron.szabo@gmail.com>
2019-10-01 10:56:04 +0200

    UPGRADE: restart game, .txt file added

--- jatek1.py
+++ jatek1.py
@@ -4,19 +4,17 @@ from typing import List
 
 WORD_LIST = open("/home/erik/Suli/het2/hangman/countries.txt").readlines()
 AVAILABLE_CHARACTERS = "qwertyuiopasdfghjklzx&cvbnm-"
-UNKNOWN_WORD = random.choice(WORD_LIST).lower().strip()
-# choosing unknown_word WORKS!
-GUESS_WORD = []
-length_word = len(UNKNOWN_WORD)
-health = 6
-already_used = []
+
+
+
 
 #------------------------------------------
 
 #--------------------------------------------
 
 
-#will change unknown_word ---> "---------"
+#will change unknown_word
+# ---> "---------"
 
 
 #----------------------------------
@@ -42,112 +40,132 @@ already_used = []
 
 
 
-for character in UNKNOWN_WORD:
-    GUESS_WORD.append("-")
 
-print()
-print(f"Word to guess: ")
 
 
 
 
 #---------------------------------------
 
-#def hangman_body_function():
+def hangman_body():
+    unknown_word = random.choice(WORD_LIST).lower().strip()
+    guess_word = []
+    length_word = len(unknown_word)
+    health = 6
+    already_used = []
+    for character in unknown_word:
+        guess_word.append("-")
 
-
-while health != 0 or guess == UNKNOWN_WORD:
-    print("**************************************")
-    print(f"Length of word = {length_word}")
-    print(f"Current health: {health}")
-    print(f"Already used characters: {already_used}")
-    print("**************************************")
-    print(GUESS_WORD)
-    guess = input("Guess a letter (or a '-'):\n")
-    guess = guess.lower()
-    if not guess in AVAILABLE_CHARACTERS:
-        print("Please type any single letter of the alphabet (or '&' or '-')!")
-    elif guess in already_used:
-        print("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
-        print("!YOU ALREADY USED THAT LETTER!")
-        print("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
-    else:
-        already_used.append(guess)
-        if guess in UNKNOWN_WORD:
-            print("\n\n")
-            print("You guessed correctly!")
-            for i in range(0, length_word):
-                if UNKNOWN_WORD[i] == guess:
-                    GUESS_WORD[i] = guess
-            print(GUESS_WORD)
-            if not "-" in GUESS_WORD:
-                print("******************************")
-                print("**********YOU WON!************")
-                print("******************************")
-
-                # start a new game?
-                print("\nGAME OVER!")
-                break
+    print()
+    print(f"Word to guess: ")
+    while health != 0 or guess == unknown_word:
+        print("**************************************")
+        print(f"Length of word = {length_word}")
+        print(f"Current health: {health}")
+        print(f"Already used characters: {already_used}")
+        print("**************************************")
+        print(guess_word)
+        guess = input("Guess a letter (or a '-'):\n")
+        guess = guess.lower()
+        if not guess in AVAILABLE_CHARACTERS:
+            print("Please type any single letter of the alphabet (or '&' or '-')!")
+        elif guess in already_used:
+            print("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
+            print("!YOU ALREADY USED THAT LETTER!")
+            print("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
         else:
-            print("That letter is not in the word. Try again!")
-            if guess not in UNKNOWN_WORD:
+            already_used.append(guess)
+            if guess in unknown_word:
+        
+                print("\n\n")
+                print("You guessed correctly!")
+                for i in range(0, length_word):
+                    if unknown_word[i] == guess:
+                        guess_word[i] = guess
+                print(guess_word)
+                if not "-" in guess_word:
+                    print("******************************")
+                    print("**********YOU WON!************")
+                    print("******************************")
+
+                    # start a new game?
+                    print("\nGAME OVER!")
+                    break
+            else:
+                print("That letter is not in the word. Try again!")
+                if guess not in unknown_word:
+                    
+                    health -= 1
+                    if health == 5:
+                        print(" -----")
+                        print(" |    |")
+                        print(" |    0")
+                        print(" |   ")  
+                        print("___  ")
+                        print()
+                        print(already_used)
+                    elif health == 4:
+                        print(" -----")
+                        print(" |    |")
+                        print(" |    0")
+                        print(" |    I")  
+                        print("___  ")
+                        print()
+                        print(already_used)
+                    elif health == 3:
+                        print(" -----")
+                        print(" |    |")
+                        print(" |    0")
+                        print(" |   -I")  
+                        print("___  ")
+                        print()
+                        print(already_used)
+                    elif health == 2:
+                        print(" -----")
+                        print(" |    |")
+                        print(" |    0")
+                        print(" |   -I-")  
+                        print("___  ")
+                        print()
+                        print(already_used)
+                    elif health == 1:
+                        print(" -----")
+                        print(" |    |")
+                        print(" |    0")
+                        print(" |   -I-")  
+                        print("___  /")
+                        print()
+                        print(already_used)
+                    elif health == 0:
+                        print(" -----")
+                        print(" |    |")
+                        print(" |    0")
+                        print(" |  --I--")  
+                        print("___  / \ ")
+                        print()
+                        print(already_used)
+                        print()
+                        print("You lose!")
+                        print()
+                        print(f"The word was -----> {unknown_word}")
+def restart_game():
+    while True:
+        user_choice = input("Do you want to start a new game? (Y/N)\n")
+        try:
+            if user_choice == "Y" or user_choice == "y":
+                print("Starting a new game...")
+                hangman_body()
+            elif user_choice == "N" or user_choice == "n":
+                break
+                exit()
                 
-                health -= 1
-                if health == 5:
-                    print(" -----")
-                    print(" |    |")
-                    print(" |    0")
-                    print(" |   ")  
-                    print("___  ")
-                    print()
-                    print(already_used)
-                elif health == 4:
-                    print(" -----")
-                    print(" |    |")
-                    print(" |    0")
-                    print(" |    I")  
-                    print("___  ")
-                    print()
-                    print(already_used)
-                elif health == 3:
-                    print(" -----")
-                    print(" |    |")
-                    print(" |    0")
-                    print(" |   -I")  
-                    print("___  ")
-                    print()
-                    print(already_used)
-                elif health == 2:
-                    print(" -----")
-                    print(" |    |")
-                    print(" |    0")
-                    print(" |   -I-")  
-                    print("___  ")
-                    print()
-                    print(already_used)
-                elif health == 1:
-                    print(" -----")
-                    print(" |    |")
-                    print(" |    0")
-                    print(" |   -I-")  
-                    print("___  /")
-                    print()
-                    print(already_used)
-                elif health == 0:
-                    print(" -----")
-                    print(" |    |")
-                    print(" |    0")
-                    print(" |  --I--")  
-                    print("___  / \ ")
-                    print()
-                    print(already_used)
-                    print()
-                    print("You lose!")
-                    print()
-                    print(f"The word was -----> {UNKNOWN_WORD}")
+        except:
+            print("Please type 'y' (yes) or 'n' (no)!")   
 
 
-#if __name__ == "__main__":
-    
 
+
+if __name__ == "__main__":
+    hangman_body()
+    restart_game()
 
